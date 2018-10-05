e1bf0141107ad379c784471ce2edc099c2f50c85
Benny <bcheng96@umd.edu>
2018-10-05 15:23:49 -0400

    upload week_05 and week_06 class code

--- lectures/week_06-expressjs-2/code/completed/index.js
+++ lectures/week_06-expressjs-2/code/completed/index.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var logger = require('morgan');
 var operations = require("./operations");
+
+var exphbs = require('express-handlebars');
 
 var contacts = {}
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    res.render('first');
 })
 
 app.get('/factorial', function(req, res) {
@@ -36,31 +43,46 @@ app.get('/sqroot', function(req, res) {
     res.send(operations.sqroot(number) + "");
 })
 
+var past = [];
+
 app.get("/operation/:op", function(req, res) {
     var op = req.params.op;
     var number = req.query.number;
+    var error = false;
 
-    if (!operations[op]) {
-        return res.send("Not a valid operation");
-    }
-    if (!number) {
-        return res.send("Please send a number");
+    if (!operations[op] || !number) {
+        error = true;
     }
 
     number = parseInt(number);
-    res.send(operations[op](number) + "");
+
+    if (error) {
+        past.push("BAD INPUT");
+    } else {
+        past.push(op + " on " + number + ": " + operations[op](number));
+    }
+
+    res.render('operation', {
+        oper: op,
+        inp: number,
+        result: error ? null : operations[op](number),
+        error: error,
+        past: past
+    })
 })
 
 app.get("/contacts", function(req, res) {
-	res.json(contacts);
+    res.json(contacts);
 });
 
 app.post("/newcontact", function(req, res) {
-	if(!req.body) { return res.send("No data recieved"); }
-	for(var contact in req.body) {
-		contacts[contact] = req.body[contact];
-	}
-	res.send("success");
+    if (!req.body) {
+        return res.send("No data recieved");
+    }
+    for (var contact in req.body) {
+        contacts[contact] = req.body[contact];
+    }
+    res.send("success");
 });
 
 
