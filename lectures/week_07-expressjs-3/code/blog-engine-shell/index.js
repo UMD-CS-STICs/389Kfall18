de79a1f3a1b4285d308a50e75b913797feb4528a
Chirag Shankar <chishankar@gmail.com>
2018-10-26 13:03:42 -0400

    Updated

--- lectures/week_07-expressjs-3/code/blog-engine-shell/index.js
+++ lectures/week_07-expressjs-3/code/blog-engine-shell/index.js
@@ -22,19 +22,56 @@ app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
 
 app.get("/", function(req, res) {
-    res.render("UNIMPLEMENTED");
+    var tags = dataUtil.getAllTags(_DATA);
+    res.render('home',{
+        data: _DATA,
+        tags:tags
+    });
+
 });
 
 app.get("/create", function(req, res) {
-    res.send("UNIMPLEMENTED");
+    res.render('create');
+});
+
+app.post("/create",function(req,res){
+    var body = req.body;
+
+    body.tags = body.tags.split(" ");
+    body.content = marked(body.content);
+
+    body.preview = body.content.substring(0,300);
+    body.time = moment().format("MMMM DD YYYY, h:mm a");
+
+    console.log(body);
+
+    _DATA.push(req.body);
+    dataUtil.saveData(_DATA);
+    res.redirect('/');
 });
 
 app.get('/post/:slug', function(req, res) {
-    res.send("UNIMPLEMENTED");
+    var _slug = req.params.slug;
+    var blog_post = _.findWhere(_DATA, {slug: _slug});
+    if (!blog_post) return res.render('404');
+    res.render("post",blog_post);   
 });
 
 app.get('/tag/:tag', function(req, res) {
-    res.send("UNIMPLEMENTED");
+    var _tag = req.params.tag;
+    var tags = dataUtil.getAllTags(_DATA);
+    var posts = [];
+    _DATA.forEach(function(post){
+        if (post.tags.includes(_tag)){
+            posts.push(post);
+        }
+    });
+    res.render('home',{
+        tag: _tag,
+        data: posts,
+        tags: tags
+    });    
+
 });
 
 // Start listening on port PORT
