ec1f5335862a0e453a20bf44e33a55249a320c67
Benny <bcheng96@umd.edu>
2018-11-12 09:36:14 -0500

    week 10 completed code added

--- lectures/week_10-web-sockets/code/completed/index.js
+++ lectures/week_10-web-sockets/code/completed/index.js
@@ -4,8 +4,6 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var dotenv = require('dotenv');
-var http = require("http").Server(app);
-var io = require('socket.io')(http);
 dotenv.load();
 
 app.use(bodyParser.json());
@@ -15,45 +13,45 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 mongoose.connect(process.env.MONGODB);
+mongoose.connection.on('error', function() {
+    console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+    process.exit(1);
+});
 
 var Movie = require('./models/Movie');
 
-io.on('connection', function(socket){
-  console.log('NEW connection.');
-  socket.on('disconnect', function(){
-    console.log('A user has disconnected');
-  });
-
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  })
-});
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
     res.render('chat');
 });
 
 app.get('/movies', function(req, res) {
-  Movie.find({}, function(err, movies){
-    return res.render('movies', {movies: movies});
-  })
+    Movie.find({}, function(err, movies) {
+        return res.render('movies', { movies: movies });
+    });
 });
 
 app.post('/movies', function(req, res) {
-  var genre = req.body.genre;
-  var title = req.body.title;
-  var movie = new Movie({
-    title: title,
-    genre: genre
-  })
-  movie.save(function(err) {
-    if(err) throw err;
-    io.emit('new movie', movie);
-    return res.send('Done!');
-  })
+    var genre = req.body.genre;
+    var title = req.body.title;
+    var movie = new Movie({
+        title: title,
+        genre: genre
+    });
+    movie.save(function(err) {
+        if (err) throw err;
+        io.emit('new movie', movie);
+        return res.send('Done!');
+    });
 });
 
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+});
 
 http.listen(3000, function() {
     console.log('Example app listening on port 3000!');
